0ad44da2d48ae0aff5865edca35592dcefa063ab
Clerick13 <Edge13@mail.ru>
2024-03-31 18:34:48 +0500

    Решение для пустого лабиринта (EmptyMazeTask).

--- EmptyMazeTask.cs
+++ EmptyMazeTask.cs
@@ -4,6 +4,9 @@ public static class EmptyMazeTask
 {
 	public static void MoveOut(Robot robot, int width, int height)
 	{
-		
-	}
+		while (robot.X != width-2)
+			robot.MoveTo(Direction.Right);
+		while (robot.Y != height - 2)
+            robot.MoveTo(Direction.Down);
+    }
 }
